83b7ed12c873852a99e150c0c29137e99aa17cc5
naresh-kumer08 <seervinaresh620@gmail,com>
2026-03-19 11:31:52 +0530

    Add static

--- shrishangeethajewelry/settings.py
+++ shrishangeethajewelry/settings.py
@@ -93,8 +93,9 @@ USE_TZ = True
 
 # STATIC FILES
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
