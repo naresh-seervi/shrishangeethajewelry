56415a5561700c86922ad9d7fffc6743494a0809
naresh-seervi <sirvinaresh008@gmail.com>
2026-03-21 00:06:32 +0530

    Update settings.py

--- shrishangeethajewelry/settings.py
+++ shrishangeethajewelry/settings.py
@@ -116,8 +116,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA FILES
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
